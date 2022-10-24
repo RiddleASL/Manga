d8f56218644dd2cbce5f1fb80a742a97bdc49f2c
RiddleASL <n00212142@iadt.ie>
2022-10-24 14:15:53 +0100

    Make model and first migration of Manga

---
+++ database/migrations/2022_10_24_123941_create_mangas_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mangas', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('author');
+            $table->string('genre');
+            $table->date('release_date');
+            $table->integer('chapters');
+            $table->longText('description');
+            $table->string('manga_image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mangas');
+    }
+};
